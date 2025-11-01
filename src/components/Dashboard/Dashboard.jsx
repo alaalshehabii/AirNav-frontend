867cf6940e4a37771ff0b86a197f073dfd6bc941
Denis S Dujota <denisdujota@gmail.com>
2025-11-01 13:30:40 +0300

    finish the auth template

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,24 @@
 // src/components/Dashboard/Dashboard.jsx
 
-import { useContext } from 'react';
-
+import { useContext, useEffect } from 'react';
+import * as userService from '../../services/userService'
 import { UserContext } from '../../contexts/UserContext';
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+
+   useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const fetchedUsers = await userService.index();
+        console.log(fetchedUsers);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    if (user) fetchUsers();
+  }, [user]);
+
 
   return (
     <main>
